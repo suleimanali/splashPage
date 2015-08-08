8930b92d1d08987c77e59e6488b96350a49d90d2
Suleiman Ali <suleiman.o.ali@gmail>
2015-08-08 04:21:25 -0500

    modified some texts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,6 +36,9 @@ Rails.application.configure do
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
+  config.serve_static_assets = true
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
